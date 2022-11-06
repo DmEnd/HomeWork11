8439a8abd44db1bd9ea138c633df0a222c5456b5
DmEnd <Dima_polit-sov@mail.ru>
2022-11-07 00:09:12 +0300

    Добавьте файлы проекта.

---
+++ HomeWork11/LinEquation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork11
+{
+    struct LinEquation
+    {
+        double k;
+        double b;
+
+        public LinEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public void Root()
+        {
+            if (k==0)
+            {
+                Console.WriteLine($"Решений нет.");
+            }
+            else
+            { 
+                double x = -b / k;
+                Console.WriteLine($"Корень уравнения {x}.");
+            }  
+            
+        }
+
+    }
+}
